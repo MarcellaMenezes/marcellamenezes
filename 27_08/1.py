2db896929b2031948d563f0183e549df97d47ea2
MarcellaMenezes <menezesmarcellacc@gmail.com>
2022-08-27 14:57:30 -0300

    Tipos, comentários, operações e condicionais

---
+++ 27_08/1.py
@@ -0,0 +1,32 @@
+# ------------ Tipos --------------------
+nome = 'Marcella'
+idade = 23.10
+destra = True
+
+print('Nome = '+str(type(nome)) + '\nIdade = '+str(type(idade)) + '\nDestra = '+ str(type(destra)))
+print('Nome = '+nome + '\nIdade = '+str(idade) + '\nDestra = '+ str(destra))
+
+# ------------ Comentários ------------
+'''
+oi
+'''
+# ------------ Operadores ------------
+
+print (1 + 2)   # soma
+print (1 - 2)   # subtração
+print (1 / 2)   # divisão
+print (1 // 2)  # Divisão Inteira
+print (1 % 2)   # Resto da Divisão
+print (27 ** (1/3))  # Exponenciação
+
+# ------------ Condicionais ------------
+
+if  1 == '1':
+    print ('São iguais')
+else:
+    print ('Não são iguais')  
+    
+if 1 == int('1'):
+    print ('São iguais')
+else:
+    print ('Não são iguais')  
