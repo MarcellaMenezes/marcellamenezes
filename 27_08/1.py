2a0fd7fb404630da8a0a3c86d10803d533f6f433
MarcellaMenezes <menezesmarcellacc@gmail.com>
2022-08-27 15:48:49 -0300

    Operadores de Atribuição e Lógica

--- 27_08/1.py
+++ 27_08/1.py
@@ -11,6 +11,7 @@ print('Nome = '+nome + '\nIdade = '+str(idade) + '\nDestra = '+ str(destra))
 oi
 '''
 # ------------ Operadores ------------
+print('\nOperadores')
 
 print (1 + 2)   # soma
 print (1 - 2)   # subtração
@@ -19,7 +20,8 @@ print (1 // 2)  # Divisão Inteira
 print (1 % 2)   # Resto da Divisão
 print (27 ** (1/3))  # Exponenciação
 
-# ------------ Condicionais ------------
+# ------------ Condicionais ------------ <=, >=, ==, !=
+print('\nCondicionais')
 
 if  1 == '1':
     print ('São iguais')
@@ -30,3 +32,25 @@ if 1 == int('1'):
     print ('São iguais')
 else:
     print ('Não são iguais')  
+    
+ # Operador de atribuição *=, /=, +=, -=
+print('\nOperador de atribuição')
+
+a = 3
+a **= 4
+print (a) 
+
+a /= 9
+print (a) 
+
+a %= 4
+print (a)
+
+# ----- Operadores Lógicos
+lista = [1, 2, 3, 4] 
+if 7 not in lista:
+    print('não tem na lista')
+
+print (7 == 7 or 6 == 5)
+print (7 == 7 and 6 == 5)
+print (not (7 == 7 and 6 == 5))
